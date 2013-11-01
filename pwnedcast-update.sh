32ddd980c25c122ec11973896c5a9da9dfc2b698
Chris Blake <chrisrblake93@gmail.com>
2013-10-31 23:13:52 -0500

    Update pwnedcast-update.sh
    We /cache now to mimic the stock recovery even more, and because logs all over /data get annoying

--- pwnedcast-update.sh
+++ pwnedcast-update.sh
@@ -34,9 +34,9 @@ do
 	touch /tmp/.pwnedcastOTA
 
 	# Delete any existing OTA
-	if [ -f /data/eureka_image.zip ]
+	if [ -f /cache/eureka_image.zip ]
 	then
-		rm /data/eureka_image.zip
+		rm /cache/eureka_image.zip
 	fi
 
 	# Variables used for the update check
@@ -65,15 +65,15 @@ do
 	elif [ "$Response" != "NoUpdate" ]
 	then
 		echo "PWNEDCAST-OTA: Update Found! Downloading now!"
-		busybox wget -q "$Response" -O /data/eureka_image.zip
+		busybox wget -q "$Response" -O /cache/eureka_image.zip
 		if [ $? -ne 0 ];
 		then
 			echo "PWNEDCAST-OTA: Error Downloading, Terminating!"
 			
 			# Delete the failed update if it exists
-			if [ -f /data/eureka_image.zip ]
+			if [ -f /cache/eureka_image.zip ]
 			then
-				rm /data/eureka_image.zip
+				rm /cache/eureka_image.zip
 			fi
 			
 			# Delete run file
@@ -86,7 +86,7 @@ do
 			echo "PWNEDCAST-OTA: Downloading and Verifiying MD5 Hash"
 			
 			MD5Hash="$Response.md5"
-			busybox wget -q "$MD5Hash" -O /data/eureka_image.zip.md5
+			busybox wget -q "$MD5Hash" -O /cache/eureka_image.zip.md5
 			
 			# Did MD5 Download Successfully?
 			if [ $? -ne 0 ];
@@ -99,7 +99,7 @@ do
 			else
 			
 				# Check of MD5 is OK
-				MD1=`busybox md5sum -c /data/eureka_image.zip.md5 | busybox awk '{ print $2 }'`
+				MD1=`busybox md5sum -c /cache/eureka_image.zip.md5 | busybox awk '{ print $2 }'`
 
 				# Compare MD5's
 				if [ "$MD1" != "OK" ]
@@ -108,7 +108,7 @@ do
 					echo "PWNEDCAST-OTA: Failed to verify, Deleting files and terminating."
 					
 					# Delete the failed update if it exists
-					rm /data/eureka_image.zip /data/eureka_image.zip.md5
+					rm /cache/eureka_image.zip /cache/eureka_image.zip.md5
 					
 					# Delete run file
 					rm /tmp/.pwnedcastOTA
@@ -118,7 +118,7 @@ do
 					echo "PWNEDCAST-OTA: File Verified Successfully!"
 					
 					# Delete md5 file as no need to keep it
-					rm /data/eureka_image.zip.md5
+					rm /cache/eureka_image.zip.md5
 					
 					# Delete run file
 					rm /tmp/.pwnedcastOTA
